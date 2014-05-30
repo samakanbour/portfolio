8b984585716fb9c777ed1e9a6f063fcd36a1231c
Kanbour Sama <samak@cmu.edu>
2014-05-30 07:30:50 +0300

    touch

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-window.onscroll = function (event) {
-	$(".img").css({
+// window.onscroll = function (event) {
+// 	$(".img").css({
+// 		"background-position": "0 " + $(event.target).scrollTop() * (-0.2) + "px"
+// 	});
+// }
+
+$('window').on('touchmove', function(event) {
+    event.preventDefault();
+    $(".img").css({
 		"background-position": "0 " + $(event.target).scrollTop() * (-0.2) + "px"
 	});
-}
+});
